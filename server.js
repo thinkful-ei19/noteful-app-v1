0bcd1c76efe54076c931a6d6491f8b5748924a57
Chris Klanac <cklanac@users.noreply.github.com>
2018-03-05 09:02:22 -0500

    Remove simDB
    Remove simDB from day 1. Use raw array methods instead.

--- server.js
+++ server.js
@@ -4,8 +4,6 @@ const express = require('express');
 
 // Simple In-Memory Database
 const data = require('./db/notes');
-const simDB = require('./db/simDB');
-const notes = simDB.initialize(data);
 
 // Create an Express application
 const app = express();
@@ -15,20 +13,50 @@ app.use(express.static('public'));
 
 // Get All (and search by query)
 app.get('/api/notes', (req, res) => {
-  const { searchTerm } = req.query;
+  
+  // Send JSON response (data is an array of objects)
+  // res.json(data);
 
-  notes.filter(searchTerm, (err, list) => {
-    res.json(list);
-  });
+  /**
+   * BONUS CHALLENGE SOLUTION: Implement Search
+   * Below are 2 solutions: verbose and terse. They are functionally identical but use different syntax
+   * 
+   * Destructure the query string property in to `searchTerm` constant 
+   * If searchTerm exists...
+   * then `filter` the data array where title `includes` the searchTerm value
+   * otherwise return `data` unfiltered
+   */
+
+  // Verbose solution
+  const searchTerm = req.query.searchTerm;
+  if (searchTerm) {
+    let filteredList = data.filter(function(item) {
+      return item.title.includes(searchTerm);
+    });
+    res.json(filteredList);
+  } else {
+    res.json(data);
+  }
+
+  // Terse solution
+  // const { searchTerm } = req.query;
+  // res.json(searchTerm ? data.filter(item => item.title.includes(searchTerm)) : data);
+
 });
 
 // Get a single item
 app.get('/api/notes/:id', (req, res) => {
   const id = req.params.id;
-
-  notes.find(id, (err, item) => {
-    res.json(item);
+  
+  // Verbose solution
+  let note = data.find(function(item) {
+    return item.id === Number(id);
   });
+  res.json(note);
+  
+  // Terse solution
+  // res.json(data.find(item => item.id === Number(id)));
+
 });
 
 // Listen for incoming connections
