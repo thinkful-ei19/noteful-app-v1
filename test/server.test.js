1b4edc0312324c6df2bff42c8abc01b4581788b0
Chris Klanac <cklanac@users.noreply.github.com>
2018-03-09 14:01:03 -0500

    breaking test

--- test/server.test.js
+++ test/server.test.js
@@ -16,7 +16,7 @@ chai.use(chaiHttp);
 describe('Reality check', function () {
 
   it('true should be true', function () {
-    expect(true).to.be.true;
+    expect(true).to.be.false;
   });
 
   it('2 + 2 should equal 4', function () {
